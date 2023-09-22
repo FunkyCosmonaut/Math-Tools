b5e70235fdc33cfb6d83d274faab30d9555b8ba2
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2023-09-22 01:06:03 -0800

    generalized

--- eulersmethod.cpp
+++ eulersmethod.cpp
@@ -17,8 +17,15 @@ double newtonCoolingLaw(double, double t)
 {  
       return -0.07 * (t-20);
 }
+double functiontwo(double x, double y)
+{
+    return 2*x - 3 * y + 1;
+}
 
 int main()
 {
-    euler(newtonCoolingLaw, 100, 0, 100, 2);
+    //euler(newtonCoolingLaw, 100, 0, 5, 1);
+    //
+    //in order to determine y(1) = 3 for example, y0 needs to equal 3 and a needs to equal 1;
+    euler(functiontwo, 3, 1, 1.2, 0.05);
 }
